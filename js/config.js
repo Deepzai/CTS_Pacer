88f2d34ae471e4bca935cf06b9771155afe170d0
Jason Liu <jasonningliu@gmail.com>
2018-06-15 13:22:30 -0600

    Added require.js library with configuration and organized library directories

---
+++ js/config.js
@@ -0,0 +1,6 @@
+require.config({
+    baseUrl: '',
+    paths: {
+       jquery: 'lib/jquery-3.3.1.min.js',
+    }
+});
--- js/pacer.js
+++ js/pacer.js
@@ -1,54 +1,55 @@
-$(document).ready(function() {
-    $("#submitInfo").on("click", function() {
-        var currentPercentage = parseInt($("#currentPercentage").val(), 10);
-        var outputText = "You are currently: " + currentPercentage + "%";
-        var validInput = true;
+require(['config'], function() {
+    $(document).ready(function() {
+        $("#submitInfo").on("click", function() {
+            var currentPercentage = parseInt($("#currentPercentage").val(), 10);
+            var outputText = "You are currently: " + currentPercentage + "%";
+            var validInput = true;
 
-        switch(true) {
-            case isNaN(currentPercentage):
-                alterAlert(".output_div", "alert-primary", "alert-danger");
-                outputText = "You must enter a valid number (0-100).";
-                validInput = false;
-                break;
-            case (currentPercentage < 0):
-                alterAlert(".output_div", "alert-primary", "alert-danger");
-                outputText = "You must enter a positive number.";
-                validInput = false;
-                break;
-            case (currentPercentage > 100):
-                alterAlert(".output_div", "alert-primary", "alert-danger");
-                outputText = "You must enter a number between 0-100.";
-                validInput = false;
-                break;
-        }
+            switch(true) {
+                case isNaN(currentPercentage):
+                    alterAlert(".output_div", "alert-primary", "alert-danger");
+                    outputText = "You must enter a valid number (0-100).";
+                    validInput = false;
+                    break;
+                case (currentPercentage < 0):
+                    alterAlert(".output_div", "alert-primary", "alert-danger");
+                    outputText = "You must enter a positive number.";
+                    validInput = false;
+                    break;
+                case (currentPercentage > 100):
+                    alterAlert(".output_div", "alert-primary", "alert-danger");
+                    outputText = "You must enter a number between 0-100.";
+                    validInput = false;
+                    break;
+            }
 
-        if (validInput) {
-            alterAlert(".output_div", "alert-danger", "alert-success");
-            animate('#confirm_text', "Is this correct? ", 1250)
-            append_confirmation();
-            $("#submitInfo").hide();
-        }
+            if (validInput) {
+                alterAlert(".output_div", "alert-danger", "alert-success");
+                animate('#confirm_text', "Is this correct? ", 1250)
+                append_confirmation();
+                $("#submitInfo").hide();
+            }
 
-        animate('#output_text', outputText, 800);
+            animate('#output_text', outputText, 800);
 
-        $('#btn_yes').on("click", function() {
-            $("#btn_yes").fadeOut(700);
-            $("#btn_no").fadeOut(700);
-            $("#confirm_text").fadeOut(600);
-            animate('#output_text', "Okay. Working on it...", 1500);
-            $('.output_div').addClass("breathing");
-            //generate table
+            $('#btn_yes').on("click", function() {
+                $("#btn_yes").fadeOut(700);
+                $("#btn_no").fadeOut(700);
+                $("#confirm_text").fadeOut(600);
+                animate('#output_text', "Okay. Working on it...", 1500);
+                $('.output_div').addClass("breathing");
 
-            // ajax call to where Video Names are saved
-            $.ajax({
-                url:'https://www.dropbox.com/s/4r1scr8y5x0ef5f/cts_2018.txt?raw=1',
-                success: function (data) {
-                    videos = data.split("\n");
-                    //videos contains all the data (n-1 where n is video #)
-                    alert(videos[322-1])
-                }
+                // ajax call to where Video Names are saved
+                $.ajax({
+                    url:'https://www.dropbox.com/s/4r1scr8y5x0ef5f/cts_2018.txt?raw=1',
+                    success: function (data) {
+                        videos = data.split("\n");
+                        //videos contains all the data (n-1 where n is video #)
+                        alert(videos[322-1])
+                        //generate table while you have all the data
+                    }
+                });
             });
-            
         });
     });
 });
@@ -69,13 +70,4 @@ function append_confirmation() {
 function alterAlert(target, remove, add) {
     $(target).removeClass(remove);
     $(target).addClass(add);
-}
-
-function outputText(obj) {
-    $.ajax({
-        url:'https://www.dropbox.com/s/4r1scr8y5x0ef5f/cts_2018.txt?raw=1',
-        success: function (data) {
-            obj = data;
-        }
-    });
 }
