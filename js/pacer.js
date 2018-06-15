237625212365c248e3946df1c5b25fe827eb0d5f
Jason Liu <jasonningliu@gmail.com>
2018-06-15 01:12:29 -0600

    Added transition affect after input

--- js/pacer.js
+++ js/pacer.js
@@ -1,11 +1,41 @@
 $(document).ready(function() {
     $("#submitInfo").on("click", function(){
         var currentPercentage = parseInt($("#currentPercentage").val(), 10);
+        var outputText = "You are currently: " + currentPercentage + "%";
+        var validInput = true;
 
-        var data = ((isNaN(currentPercentage) || currentPercentage < 0)
-        ? "stop breaking me. enter a proper number."
-        : ("Currently: " + currentPercentage + "%"))
+        switch(true) {
+            case isNaN(currentPercentage):
+                outputText = "You must enter a valid number (0-100).";
+                validInput = false;
+                break;
+            case (currentPercentage < 0):
+                outputText = "You must enter a positive number.";
+                validInput = false;
+                break;
+            case (currentPercentage > 100):
+                outputText = "You must enter a number between 0-100.";
+                validInput = false;
+                break;
+        }
+        animate('#output_text', outputText, "slow");
 
-        $('#output').text(data);
+        if (validInput) {
+            animate('#confirm_text', "Is this correct? ", "slow")
+            append_confirmation();
+            $("#submitInfo").toggle();
+        }
     });
 });
+
+function animate(target, output, speed) {
+    $(target).hide();
+    $(target).text(output);
+    $(target).fadeIn(speed);
+}
+
+function append_confirmation() {
+    var element1 = "<button class='button1'>Yes</button>"
+    var element2 = "<button class='button1'>No</button>";
+    $('.confirm_div').append(element1, element2);
+}
